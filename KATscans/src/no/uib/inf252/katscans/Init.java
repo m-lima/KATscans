442218c08f7c847c22a547645433033ff9e3ac4c
m-lima <marcelowind@gmail.com>
2016-02-14 15:39:59 +0100

    Initial files

---
+++ KATscans/src/no/uib/inf252/katscans/Init.java
@@ -0,0 +1,15 @@
+package no.uib.inf252.katscans;
+
+/**
+ *
+ * @author Marcelo Lima
+ */
+public class Init {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
