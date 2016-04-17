9cd442d255eea10f87153840f1ebf847b6068018
m-lima <marcelowind@gmail.com>
2016-04-17 20:10:21 +0200

    More efficient raytracing

--- src/main/resources/shaders/raycaster.fs
+++ src/main/resources/shaders/raycaster.fs
@@ -1,65 +1,66 @@
 in vec3 vertexOut;
-in vec3 vertexOutModel;
+in vec4 vertexOutModel;
 
 uniform sampler3D volumeTexture;
 
 uniform int numSamples;
-uniform mat3 model;
+uniform mat4 model;
 uniform bool orthographic;
 uniform vec3 eyePos;
+uniform vec3 ratio;
 
-const float stepSize = 2.0 * sqrt(2.0) / float(numSamples);
+const float stepSize = sqrt(3.0) / float(numSamples);
 const float densityFactor = 8.0;
 const float absorption = 1.0;
 
-void main()
-{       
+void main() {       
     vec3 effectiveEyePos = eyePos;
     if (orthographic) {
         effectiveEyePos.xy = vertexOutModel.xy;
     } 
 
-    effectiveEyePos = inverse(model) * effectiveEyePos;
+    effectiveEyePos = (inverse(model) * vec4(effectiveEyePos, 1.0)).xyz;
     vec3 rayDirection = normalize(vertexOut - effectiveEyePos);
 
-    vec3 invRay = 1.0 / rayDirection;
-    vec3 back = invRay * (-(1.0 + effectiveEyePos));
-    vec3 front = invRay * (1.0 - effectiveEyePos);
-    vec3 tmin = min(back, front);
-    vec3 tmax = max(back, front);
+    vec3 pos = vertexOut;
+    vec3 stepValue = rayDirection * stepSize;
 
-    vec2 temp = max(tmin.xx, tmin.yz);
-    float near = max(temp.x, temp.y);
-    if (near < 0.0) near = 0.0;
-
-    temp = min(tmax.xx, tmax.yz);
-    float far = min(temp.x, temp.y);
-
-    vec3 rayStart = effectiveEyePos + rayDirection * near;
-    vec3 rayStop = effectiveEyePos + rayDirection * far;
-
-    vec3 pos = rayStart;
-    vec3 stepValue = normalize(rayStop - rayStart) * stepSize;
-    float travel = distance(rayStop, rayStart);
-
-    float colorOut = 0.0;
+    vec4 colorOut;
     float density;
-    for (int i = 0; i < numSamples && travel > 0.0; ++i, pos += stepValue, travel -= stepSize) {
-        density = texture(volumeTexture, pos * 0.5 + 0.5).x * densityFactor;
-        if (density <= 0.0) continue;
-        //colorOut += density * stepSize;
-        colorOut = max(density, colorOut);
-        if (colorOut >= 1.0) break;
+    vec3 coord;
+    int found = 0;
+    for (int i = 0; i < numSamples; ++i, pos += stepValue) {
+        coord = pos * ratio + 0.5;
+        if (coord.x < 0.0 || coord.x > 1.0 ||
+            coord.y < 0.0 || coord.y > 1.0 ||
+            coord.z < 0.0 || coord.z > 1.0) {
+            break;
+        }
+        density = texture(volumeTexture, coord).x;
+        /*if (density >= 0.0015 && density <= 0.0017) {
+            colorOut = vec4(1, 0, 0, 1);
+            break;
+        }*/
+        if (density >= 0.017 && density <= 0.02) {
+            //colorOut = vec4(0, 1, 0, 1);
+            //break;
+            found++;
+        }
+        //if (density <= 0.0) continue;
+        ////colorOut += density * stepSize;
+        //colorOut = max(density, colorOut);
+        //if (colorOut >= 1.0) break;
     }
 
-    gl_FragColor.rgb = vec3(colorOut);
-    if (colorOut < 0.1) colorOut = 0.0;
-    gl_FragColor.a = colorOut;
+    gl_FragColor = vec4(0, 1, 0, found * 0.15);
+    //gl_FragColor.rgb = vec3(colorOut);
+    //if (colorOut < 0.1) colorOut = 0.0;
+    //gl_FragColor.a = colorOut;
 
 //#define COLOR_CUBE
 #ifdef COLOR_CUBE
-    float minLimit = -1.0;
-    float maxLimit = 1.0;
+    float minLimit = -0.5;
+    float maxLimit = 0.5;
     vec4 saturated = vec4((vertexOut.x == maxLimit || vertexOut.x == minLimit) ? 1.0 : 0.0,
                           (vertexOut.y == maxLimit || vertexOut.y == minLimit) ? 1.0 : 0.0,
                           (vertexOut.z == maxLimit || vertexOut.z == minLimit) ? 1.0 : 0.0,
--- src/main/resources/shaders/raycaster.vs
+++ src/main/resources/shaders/raycaster.vs
@@ -1,15 +1,17 @@
 layout (location = 0) in vec3 position;
 
 out vec3 vertexOut;
-out vec3 vertexOutModel;
+out vec4 vertexOutModel;
 
+uniform vec3 ratio;
 uniform mat4 projection;
 uniform mat4 view;
-uniform mat3 model;
+uniform mat4 model;
 
 void main()
 {
-    vertexOut = position * 2.0 - 1.0;
-    vertexOutModel = model * vertexOut;
-    gl_Position = projection * view * vec4(vertexOutModel, 1.0);
+    vertexOut = position - 0.5;
+    vertexOut /= ratio;
+    vertexOutModel = model * vec4(vertexOut, 1.0);
+    gl_Position = projection * view * vertexOutModel;
 }
